406ed65df92c0c055d35249cb97603e9333b5569
Lito Bumba <litocdbumba@gmail.com>
2024-11-16 02:16:08 -0300

    Project running with basic initial counter state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from "vue";
+
+let counter = ref(0)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ counter }}</h1>
+    <button @click="counter++">Click</button>
   </main>
 </template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  font-size: 16rem;
+  text-align: center;
+  font-weight: bold;
+}
+
+button {
+  border: none;
+  background: white;
+  padding: 8px 16px;
+  outline: none;
+  cursor: pointer;
+  border-radius: 8px;
+  font-weight: bolder;
+}
+</style>
